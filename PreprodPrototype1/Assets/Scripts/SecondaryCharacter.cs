33d5834744c2e74b1ea843fb6b28c7112c2d514d
Luio-MDS <luio.anolen@mds.ac.nz>
2026-03-10 22:35:13 +1300

    added SecondaryCharacter script

--- PreprodPrototype1/Assets/Scripts/SecondaryCharacter.cs
+++ PreprodPrototype1/Assets/Scripts/SecondaryCharacter.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework.Internal.Commands;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,55 @@ using UnityEngine;
 public class SecondaryCharacter : MonoBehaviour
 {
     public enum secondaryCharacter { Researcher, Devotee, Mercenary, Knight};
+    private Player player;
+    public int regenAmount = 2;
+
+    //effect
+    private bool shouldShake;
+    private float shakeSpeed = 2.0f;
+    private float shakeAmount = 1.5f;
+
+    void Start()
+    {
+        SetPlayer();
+    }
+
+    void Update()
+    {
+        if (shouldShake)
+        {
+            ObjShake();
+        }
+    }
+
+    void Regeneration()
+    {
+        if (player == null)
+        {
+            SetPlayer();
+        }
+
+        player.Heal(regenAmount);
+        TurnManager.Instance.UpdateMoveText(Color.green, "+ " + regenAmount.ToString());
+    }
+
+    void SetPlayer()
+    {
+        player = FindFirstObjectByType<Player>();
+    }
+
+    void ObjShake()
+    {
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Sin(Time.time * shakeSpeed) * shakeAmount;
+        transform.position = pos;
+
+        // set a timer to stop shaking
+        float timer = Time.deltaTime;
+        while (timer < 1)
+        {
+            timer += Time.deltaTime;
+        }
+        shouldShake = false;
+    }
 }
